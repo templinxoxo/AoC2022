8bdf867896e7ac32911395df8c1aeb41acc714a3
Michal Templin <michaltemplin@surferseo.com>
2022-12-01 12:30:43 +0100

    day1: part2 - find top 3 most calory in inventories

--- lib/days_01_10/day1.ex
+++ lib/days_01_10/day1.ex
@@ -1,16 +1,33 @@
 defmodule Day1 do
+  # execute methods
   def execute_part1() do
     get_elf_calory_list()
     |> parse_calory_list()
     |> find_most_calory_inventory()
   end
 
+  def execute_part2() do
+    get_elf_calory_list()
+    |> parse_calory_list()
+    |> find_top_three_most_calory_inventories()
+  end
+
+  # actual logic
   def find_most_calory_inventory(elves_inventories) do
     elves_inventories
     |> Enum.map(&Enum.sum(&1))
     |> Enum.max()
   end
 
+  def find_top_three_most_calory_inventories(elves_inventories) do
+    elves_inventories
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  # helpers
   def get_elf_calory_list() do
     Api.get_input(1)
   end
--- test/days_1_10/day1_test.exs
+++ test/days_1_10/day1_test.exs
@@ -20,12 +20,21 @@ defmodule Day1Test do
   10000
   """
 
-  test "Part 1 - top 1 most caloric inventory" do
+  test "Part 1" do
     part1 =
       @test_data
       |> parse_calory_list()
       |> find_most_calory_inventory()
 
     assert part1 == 24_000
+  end
+
+  test "Part 2" do
+    part1 =
+      @test_data
+      |> parse_calory_list()
+      |> find_top_three_most_calory_inventories()
+
+    assert part1 == 45_000
   end
 end
