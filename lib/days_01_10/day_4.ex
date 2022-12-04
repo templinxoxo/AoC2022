aec15ccea4b06be86d6a78e043b240c334a2035b
Michal Templin <michaltemplin@surferseo.com>
2022-12-04 11:03:27 +0100

    day4: part2 - get overlapping assignments

--- lib/days_01_10/day_4.ex
+++ lib/days_01_10/day_4.ex
@@ -6,6 +6,12 @@ defmodule Day4 do
     |> get_fully_contained_assignments()
   end
 
+  def execute_part2() do
+    get_data()
+    |> parse_data()
+    |> get_overlapping_assignments()
+  end
+
   # actual logic
   def get_fully_contained_assignments(groups) do
     groups
@@ -17,6 +23,21 @@ defmodule Day4 do
         |> Tuple.to_list()
 
       total_range in assignments
+    end)
+    |> Enum.count()
+  end
+
+  def get_overlapping_assignments(groups) do
+    IO.inspect(length(groups))
+    # one assignment sections must close before other one begins to avoid overlap
+    # so, possible overlap is from 2nd section start to 1st section end
+    groups
+    |> Enum.filter(fn assignments ->
+      [[_, possible_overlap_end], [possible_overlap_start, _]] =
+        assignments
+        |> Enum.sort_by(&Enum.at(&1, 0))
+
+      possible_overlap_end >= possible_overlap_start
     end)
     |> Enum.count()
   end
--- test/days_1_10/day4_test.exs
+++ test/days_1_10/day4_test.exs
@@ -7,7 +7,7 @@ defmodule Day4Test do
   2-4,6-8
   2-3,4-5
   5-7,7-9
-  2-8,3-7
+  3-7,2-8
   6-6,4-6
   2-6,4-8
   """
@@ -19,5 +19,14 @@ defmodule Day4Test do
       |> get_fully_contained_assignments()
 
     assert part1 == 2
+  end
+
+  test "Part 1 - get overlapping assignments number" do
+    part1 =
+      @test_data
+      |> parse_data()
+      |> get_overlapping_assignments()
+
+    assert part1 == 4
   end
 end
