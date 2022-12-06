3cdca23201b7992928552c2924f0d3ae15f42b66
Michal Templin <michaltemplin@surferseo.com>
2022-12-06 07:47:45 +0100

    day6: part2 - find 14 characters starter packet

--- lib/days_01_10/day_6.ex
+++ lib/days_01_10/day_6.ex
@@ -5,19 +5,25 @@ defmodule Day6 do
     |> find_start_marker()
   end
 
+  # execute methods
+  def execute_part2() do
+    get_data()
+    |> find_start_marker(14)
+  end
+
   # actual logic
-  def find_start_marker(datastream) do
+  def find_start_marker(datastream, masker_size \\ 4) do
     datastream
     |> String.split("", trim: true)
-    |> Enum.chunk_every(4, 1, :discard)
+    |> Enum.chunk_every(masker_size, 1, :discard)
     |> Enum.with_index()
     |> Enum.find(fn {sequence, _index} ->
       sequence
       |> Enum.uniq()
-      |> Enum.count() == 4
+      |> Enum.count() == masker_size
     end)
     |> case do
-      {_, index} -> index + 4
+      {_, index} -> index + masker_size
       _ -> 0
     end
   end
--- test/days_1_10/day6_test.exs
+++ test/days_1_10/day6_test.exs
@@ -18,4 +18,12 @@ defmodule Day6Test do
 
     assert part1 == [7, 5, 6, 10, 11]
   end
+
+  test "Part 2 - find 14 character start packet" do
+    part1 =
+      @test_data
+      |> Enum.map(&find_start_marker(&1, 14))
+
+    assert part1 == [19, 23, 23, 29, 26]
+  end
 end
