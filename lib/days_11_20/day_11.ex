625d71c47d373bf25fd08f13d73e94a686ecde43
Michal Templin <michaltemplin@surferseo.com>
2022-12-11 10:53:50 +0100

    day11: part1 - count monkey business in 20 rounds

---
+++ lib/days_11_20/day_11.ex
@@ -0,0 +1,110 @@
+defmodule Day11 do
+  # execute methods
+  def execute_part1() do
+    get_data()
+    |> parse_monkey_notes()
+    |> get_throws_in_time(20)
+    |> count_monkey_business()
+  end
+
+  # actual logic
+  def count_monkey_business(throws_history) do
+    [{_monkey1, monkey1_score}, {_monkey2, monkey2_score}] =
+      throws_history
+      |> Enum.group_by(&elem(&1, 0))
+      |> Enum.map(fn {number, throws} -> {number, length(throws)} end)
+      |> Enum.sort_by(&elem(&1, 1), :desc)
+      |> IO.inspect()
+      |> Enum.take(2)
+
+    monkey1_score * monkey2_score
+  end
+
+  def get_throws_in_time({monkeys, items}, rounds, relif_divider \\ 1) do
+    {_, throws_history} =
+      1..rounds
+      |> Enum.reduce({items, []}, fn _round, {round_items, history} ->
+        {next_round_items, round_history} =
+          throw_during_single_round(round_items, monkeys, relif_divider)
+
+        {
+          next_round_items,
+          history ++ round_history
+        }
+      end)
+
+    throws_history
+  end
+
+  def throw_during_single_round(round_items, monkeys, relif_divider) do
+    monkeys
+    |> Enum.reduce({round_items, []}, fn monkey, {items, history} ->
+      {current_monkey_items, other_items} = Enum.split_with(items, &(elem(&1, 0) == monkey.index))
+
+      thrown_items = current_monkey_items |> Enum.map(&throw_item(&1, monkey))
+
+      {other_items ++ thrown_items, history ++ current_monkey_items}
+    end)
+  end
+
+  def throw_item({_, item}, monkey) do
+    new_worry = floor(calculate_worry(item, monkey.operation) / 3)
+
+    if rem(new_worry, monkey.test) == 0 do
+      {monkey.if_true, new_worry}
+    else
+      {monkey.if_false, new_worry}
+    end
+  end
+
+  def calculate_worry(item, "* old"), do: item * item
+  def calculate_worry(item, "* " <> multiplier), do: item * String.to_integer(multiplier)
+  def calculate_worry(item, "+ " <> value), do: item + String.to_integer(value)
+
+  # helpers
+  def get_data() do
+    Api.get_input(11)
+  end
+
+  def parse_monkey_notes(data) do
+    monkey_notes =
+      data
+      |> String.split("\n\n", trim: true)
+      |> Enum.with_index()
+      |> Enum.map(fn {monkey, index} ->
+        {items, operation, test, mokey_true, mokey_false} = get_monkey_details(monkey)
+
+        %{
+          items: [
+            items
+            |> String.split(",", trim: true)
+            |> Enum.map(&{index, &1 |> String.trim() |> String.to_integer()})
+          ],
+          monkey: %{
+            index: index,
+            operation: operation,
+            test: String.to_integer(test),
+            if_true: String.to_integer(mokey_true),
+            if_false: String.to_integer(mokey_false)
+          }
+        }
+      end)
+
+    items = monkey_notes |> Enum.map(& &1.items) |> List.flatten()
+    monkeys = monkey_notes |> Enum.map(& &1.monkey) |> Enum.sort_by(& &1.index, :asc)
+    {monkeys, items}
+  end
+
+  def get_monkey_details(monkey) do
+    [
+      _,
+      "Starting items:" <> items,
+      "Operation: new = old " <> operation,
+      "Test: divisible by " <> test,
+      "If true: throw to monkey " <> mokey_true,
+      "If false: throw to monkey " <> mokey_false
+    ] = monkey |> String.split("\n", trim: true) |> Enum.map(&String.trim(&1))
+
+    {items, operation, test, mokey_true, mokey_false}
+  end
+end
---
+++ test/days_11_20/day11_test.exs
@@ -0,0 +1,45 @@
+defmodule Day11Test do
+  use ExUnit.Case
+  doctest Day11
+  import Day11
+
+  @test_data """
+  Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+  Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+  Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+  Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+  """
+
+  test "Part 1 - " do
+    part1 =
+      @test_data
+      |> parse_monkey_notes()
+      |> get_throws_in_time(20, 3)
+      |> count_monkey_business()
+
+    assert part1 == 10_605
+  end
+end
