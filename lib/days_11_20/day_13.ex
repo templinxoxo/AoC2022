07594572eeaa74bc7e3bd0670498fee090064551
Michal Templin <michaltemplin@surferseo.com>
2022-12-13 08:55:32 +0100

    day13: part1 - determine if packets are in right order

---
+++ lib/days_11_20/day_13.ex
@@ -0,0 +1,60 @@
+defmodule Day13 do
+  # execute methods
+  def execute_part1() do
+    get_data()
+    |> parse_data()
+    |> find_in_order_packets()
+    |> sum_ordered_packets()
+  end
+
+  # actual logic
+  def find_in_order_packets(packets) do
+    packets
+    |> Enum.with_index()
+    |> Enum.map(fn {packets, index} -> {packets, index + 1} end)
+    |> Enum.map(fn {[packet1, packet2], index} ->
+      ordered = is_ordered?(packet1, packet2)
+
+      {[packet1, packet2], index, ordered}
+    end)
+  end
+
+  def sum_ordered_packets(packets) do
+    packets
+    |> Enum.filter(fn {_packets, _index, ordered} -> ordered end)
+    |> Enum.map(fn {_packets, index, _ordered} -> index end)
+    |> Enum.sum()
+  end
+
+  def is_ordered?(p1, p2) when is_integer(p1) and is_integer(p2) do
+    cond do
+      p1 == p2 -> :continue
+      p1 > p2 -> false
+      p1 < p2 -> true
+    end
+  end
+
+  def is_ordered?([p1 | rest1], [p2 | rest2]) do
+    case is_ordered?(p1, p2) do
+      :continue -> is_ordered?(rest1, rest2)
+      ordered -> ordered
+    end
+  end
+
+  def is_ordered?([], []), do: :continue
+  def is_ordered?([], [_ | _]), do: true
+  def is_ordered?([_ | _], []), do: false
+
+  def is_ordered?(p1, p2) when is_list(p1) and is_integer(p2), do: is_ordered?(p1, [p2])
+  def is_ordered?(p1, p2) when is_integer(p1) and is_list(p2), do: is_ordered?([p1], p2)
+
+  # helpers
+  def get_data() do
+    Day13.Data.call()
+  end
+
+  def parse_data(data) do
+    data
+    |> Enum.chunk_every(2)
+  end
+end
---
+++ test/days_11_20/day13_test.exs
@@ -0,0 +1,34 @@
+defmodule Day13Test do
+  use ExUnit.Case
+  doctest Day13
+  import Day13
+
+  @test_data [
+    [1, 1, 3, 1, 1],
+    [1, 1, 5, 1, 1],
+    [[1], [2, 3, 4]],
+    [[1], 4],
+    [9],
+    [[8, 7, 6]],
+    [[4, 4], 4, 4],
+    [[4, 4], 4, 4, 4],
+    [7, 7, 7, 7],
+    [7, 7, 7],
+    [],
+    [3],
+    [[[]]],
+    [[]],
+    [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+    [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]
+  ]
+
+  test "Part 1 - find right ordered packets" do
+    part1 =
+      @test_data
+      |> parse_data()
+      |> find_in_order_packets()
+      |> sum_ordered_packets()
+
+    assert part1 == 13
+  end
+end
